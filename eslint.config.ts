4717a912771ff06d2a932c5112a83edb3ea0ec0c
Antony74 <akb@akb.me.uk>
2026-05-04 21:31:36 +0100

    add linter

---
+++ eslint.config.ts
@@ -0,0 +1,65 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+    {
+        ignores: [
+            'node_modules/',
+            'dist/',
+            'build/',
+            '.vite/',
+            'coverage/',
+        ],
+    },
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2024,
+            sourceType: 'module',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+            'react/jsx-uses-react': 'off',
+        },
+    },
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2024,
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_' },
+            ],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'no-undef': 'off', // TypeScript handles this
+        },
+    },
+];
