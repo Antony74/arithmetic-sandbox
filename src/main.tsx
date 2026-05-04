9f332b08641b790c51f90b2e2779941216efc79d
Antony74 <akb@akb.me.uk>
2026-05-04 21:53:48 +0100

    remove 'as' statement

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+const root = document.getElementById('root');
+
+if (root instanceof HTMLElement) {
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    );
+} else {
+    throw new Error(`HTMLElement with id='root' not found`);
+}
